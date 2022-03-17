0c00266db267f7e13516947ea5a00e576dbcb14c
Maxim Martynov <max.mrtnv@gmail.com>
2022-03-17 15:05:32 +0400

    Implementation: Thumbnail bg

--- src/screen/trimming/components/TrimmerBar.tsx
+++ src/screen/trimming/components/TrimmerBar.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import {Image, StyleProp, StyleSheet, View, ViewStyle} from 'react-native';
+import {
+  FlatList,
+  Image,
+  StyleProp,
+  StyleSheet,
+  View,
+  ViewStyle,
+} from 'react-native';
 
 const LeftTrimmerBorder = () => {
   return <TrimmerBorderContent style={styles.leftTrimmerBorder} />;
@@ -19,8 +26,31 @@ const TrimmerBorderContent = (props: {style: StyleProp<ViewStyle>}) => {
   );
 };
 
+const videoThumbnail = require('../../../res/images/thumbnail3.png');
+
 const SelectedArea = () => {
-  return <View style={styles.selectedArea} />;
+  return (
+    <View style={styles.selectedArea} pointerEvents="none">
+      <FlatList
+        horizontal={true}
+        contentInset={{left: 0, top: 0, right: 0, bottom: 0}}
+        data={[
+          {id: 1, thumbnail: videoThumbnail},
+          {id: 2, thumbnail: videoThumbnail},
+          {id: 3, thumbnail: videoThumbnail},
+          {id: 4, thumbnail: videoThumbnail},
+          {id: 5, thumbnail: videoThumbnail},
+          {id: 6, thumbnail: videoThumbnail},
+          {id: 7, thumbnail: videoThumbnail},
+          {id: 8, thumbnail: videoThumbnail},
+          {id: 9, thumbnail: videoThumbnail},
+        ]}
+        keyExtractor={i => i.id.toString()}
+        ItemSeparatorComponent={() => <View />}
+        renderItem={({item}) => <Image source={item.thumbnail} />}
+      />
+    </View>
+  );
 };
 
 export const TrimmerBar = () => {
@@ -37,8 +67,7 @@ const trimmerColor = '#2D6AC7';
 
 const styles = StyleSheet.create({
   container: {
-    height: 60,
-    backgroundColor: 'red',
+    height: 48,
     borderColor: trimmerColor,
     borderWidth: 2,
     marginHorizontal: 12,
@@ -62,7 +91,6 @@ const styles = StyleSheet.create({
   },
 
   selectedArea: {
-    backgroundColor: 'green',
     flex: 1,
   },
 
